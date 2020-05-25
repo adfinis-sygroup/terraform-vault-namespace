237106f4bfbf23f045fe1d65bbdf8d56a516000b
smirta <simon.nussbaum@adfinis-sygroup.ch>
2020-05-25 17:35:17 +0200

    playing around

--- main.tf
+++ main.tf
@@ -1,59 +1,48 @@
 provider "vault" {
   version = "~> 2.11"
-#  auth_login {
-#    path = "auth/approle/login"
-#
-#    parameters = {
-#      role_id   = var.login_approle_role_id
-#      secret_id = var.login_approle_secret_id
-#    }
-#  }
-  address = "http://localhost:8200"
+  #  auth_login {
+  #    path = "auth/approle/login"
+  #
+  #    parameters = {
+  #      role_id   = var.login_approle_role_id
+  #      secret_id = var.login_approle_secret_id
+  #    }
+  #  }
+  address         = var.provider_url
   skip_tls_verify = true
-  alias = "root"
+  alias           = "root"
 }
 
 resource "vault_namespace" "namespace" {
-  path = var.namespace_name
+  path     = var.namespace_name
   provider = vault.root
 }
 
 provider "vault" {
-  version = "~> 2.11"
-  address = "http://localhost:8200"
+  version         = "~> 2.11"
+  address         = provider.url
   skip_tls_verify = true
-  alias = "ns"
-  namespace = vault_namespace.namespace.path
+  alias           = "ns"
+  namespace       = vault_namespace.namespace.path
 }
 
-resource "vault_policy" "admin_policy" {
-  name = "admins"
-  policy = file("policies/admin.hcl")
-  depends_on  = [ vault_namespace.namespace ]
-  provider = vault.ns
-}
-resource "vault_policy" "provisioner_policy" {
-  name = "provisioners"
-  policy = file("policies/provisioner.hcl")
-  depends_on  = [ vault_namespace.namespace ]
-  provider = vault.ns
-}
-resource "vault_policy" "user_policy" {
-  name = "users"
-  policy = file("policies/user.hcl")
-  depends_on  = [ vault_namespace.namespace ]
-  provider = vault.ns
+resource "vault_policy" "policy" {
+  for_each   = var.namespace_policies
+  name       = "${each.value}"
+  policy     = file("policies/${each.value}")
+  depends_on = [vault_namespace.namespace]
+  provider   = vault.ns
 }
 
 resource "vault_ldap_auth_backend" "ldap" {
-    depends_on  = [ vault_namespace.namespace ]
-    provider    = vault.ns
-    path        = "${vault_namespace.namespace.path}-ldap"
-    url         = "ldaps://dc-01.example.org"
-    userdn      = "OU=Users,OU=Accounts,DC=example,DC=org"
-    userattr    = "sAMAccountName"
-    upndomain   = "EXAMPLE.ORG"
-    discoverdn  = false
-    groupdn     = "OU=Groups,DC=example,DC=org"
-    groupfilter = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
+  depends_on  = [vault_namespace.namespace]
+  provider    = vault.ns
+  path        = var.ldap_path
+  url         = var.ldap_url
+  userdn      = var.ldap_userdn
+  userattr    = var.ldap_userattr
+  upndomain   = var.ldap_upndomain
+  discoverdn  = var.ldap_discoverdn
+  groupdn     = var.ldap_groupdn
+  groupfilter = var.ldap_groupfilter
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,59 @@
 #variable login_approle_role_id {}
 #variable login_approle_secret_id {}
 
+variable provider_url {
+  type    = string
+  default = "https://localhost:8200"
+}
+
+variable namespace_configs {
+  type = list
+}
+
+variable namespace_policies {
+  type = list
+}
+
 variable namespace_name {
   type = string
+}
+
+variable ldap_path {
+  type    = string
+  default = "ldap"
+}
+
+variable ldap_url {
+  type    = string
+  default = "ldaps://dc-01.example.org"
+}
+
+variable ldap_userdn {
+  type    = string
+  default = "OU=Users,OU=Accounts,DC=example,DC=org"
+}
+
+variable ldap_userattr {
+  type    = string
+  default = "sAMAccountName"
+}
+
+variable ldap_upndomain {
+  type    = string
+  default = "EXAMPLE.ORG"
+}
+
+variable ldap_discoverdn {
+  type    = bool
+  default = false
+}
+
+variable ldap_groupdn {
+  type    = string
+  default = "OU=Groups,DC=example,DC=org"
+}
+
+variable ldap_groupfilter {
+  type    = string
+  default = "(&(objectClass=group)(member:1.2.840.113556.1.4.1941:={{.UserDN}}))"
 }
