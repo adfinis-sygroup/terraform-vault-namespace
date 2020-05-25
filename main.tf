ae774b4c76a337e436681424819ba1b60b44d78c
smirta <simon.nussbaum@adfinis-sygroup.ch>
2020-05-25 13:57:40 +0200

    disabled approle auth

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 provider "vault" {
-  auth_login {
-    path = "auth/approle/login"
-
-    parameters = {
-      role_id   = var.login_approle_role_id
-      secret_id = var.login_approle_secret_id
-    }
-  }
+  version = "~> 2.11"
+#  auth_login {
+#    path = "auth/approle/login"
+#
+#    parameters = {
+#      role_id   = var.login_approle_role_id
+#      secret_id = var.login_approle_secret_id
+#    }
+#  }
 }
 
 resource "vault_namespace" "ns1" {
